82b0ff2a45abe8d5e8f0b476873c36ef914276db
leoCyzz <leochen_sh@sina.com>
2019-04-18 11:56:07 +0800

    update event

--- src/app/edit-comp/event/event.component.ts
+++ src/app/edit-comp/event/event.component.ts
@@ -24,6 +24,7 @@ const COMP_EVENT = {
 export class EventComponent implements OnInit, OnChanges {
   @Input() config: IComponent | IPage;
   @Input() remotes: {};
+  @Input() localActions: string[];
   events: IEvent[];
   remote: { [key: string]: IRemoteAction };
   eventTypes = [];
